2c7645bdf6da67de9492ed471a5170853f7c49a5
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-08-14 13:03:27 +0530

    Time: 7 ms (72.36%), Space: 49.7 MB (58.61%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>() ;
+        for(String s : strs){
+            char[] ch = s.toCharArray() ;
+            Arrays.sort(ch) ;
+            String str = new String(ch) ;
+            map.computeIfAbsent(str, k -> new ArrayList<>()).add(s) ;
+
+        }
+        return new ArrayList<>(map.values()) ;
+    }
+}
