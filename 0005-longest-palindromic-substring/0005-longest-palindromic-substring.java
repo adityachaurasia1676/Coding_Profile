01f391343e6cf73142f3b7fd9d55ddcd5fdd6ef7
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-08-19 10:59:59 +0530

    Time: 1593 ms (9.75%), Space: 45.8 MB (42%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public String longestPalindrome(String s) {
+
+        String res = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i; j < s.length(); j++) {
+
+                if (isPalindrome(s, i, j)) {
+
+                    if (j - i + 1 > res.length()) {
+                        res = s.substring(i, j + 1);
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
