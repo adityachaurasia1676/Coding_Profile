20aa8c7a13d6f5d6aceb926ee6e409721a4a744d
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-08-20 11:37:07 +0530

    Time: 118 ms (5.36%), Space: 47.6 MB (88.8%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int c = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==0){
+                c++;
+            }
+        }
+        while(c >= 0){
+            for(int i=0;i<nums.length - 1;i++){
+                if(nums[i] == 0){
+                int temp = nums[i+1];
+                nums[i+1] = nums[i];
+                nums[i] = temp;
+                }
+            }
+            c-- ;
+        }
+    }
+}
