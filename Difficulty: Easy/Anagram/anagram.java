9c980fa5f7003176a9978d889330b1414db43562
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-08-27 10:46:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        char[] ch1 = s1.toCharArray() ;
+        char[] ch2 = s2.toCharArray() ;
+        Arrays.sort(ch1) ;
+        Arrays.sort(ch2) ;
+        return Arrays.equals(ch1,ch2) ;
+    }
+}
