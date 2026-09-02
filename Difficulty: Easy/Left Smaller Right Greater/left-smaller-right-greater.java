b657721d1cf4b03626532b72abfeb64baff823f5
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-09-02 10:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Left Smaller Right Greater/left-smaller-right-greater.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findElement(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return -1;
+
+        int[] rightMin = new int[n];
+        rightMin[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(rightMin[i + 1], arr[i]);
+        }
+
+        int leftMax = arr[0];
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] >= leftMax && arr[i] <= rightMin[i + 1]) {
+                return arr[i];
+            }
+            leftMax = Math.max(leftMax, arr[i]);
+        }
+
+        return -1;
+    }
+}
