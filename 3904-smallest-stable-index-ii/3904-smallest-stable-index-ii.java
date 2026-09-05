f55808477d8d222b464789b777220f9daa69bb3e
CHAURASIA ADITYAKUMAR <adityachaurasia1676@gmail.com>
2026-09-05 08:34:40 +0530

    Time: 6 ms (45.8%), Space: 126.6 MB (71.37%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+       int n = nums.length ;
+
+       if(n == 0) return -1 ;
+       
+       int[] prefix = new int[n] ;
+       int[] suffix = new int[n] ;
+       
+       prefix[0] = nums[0] ;
+       
+       for(int i = 1;i<n;i++){
+        prefix[i] = Math.max(prefix[i - 1], nums[i]) ;
+       }
+       
+       suffix[n - 1] = nums[n - 1] ;
+       
+        for(int i = n - 2;i>=0;i--){
+            suffix[i] = Math.min(suffix[i + 1], nums[i]) ;
+        }
+
+        for(int i = 0;i<n;i++){
+            if(prefix[i] - suffix[i] <= k){
+                return i ;
+            }
+        }
+        return -1 ;
+    }
+}
